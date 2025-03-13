67aa6e85a4488d208124b368f357a403df5126b3
Teez-Jenz <tioluwaniemmyjay@gmail.com>
2025-03-13 20:17:33 +0000

    Add files via upload

---
+++ Pages/Home/HomeScreen/Index.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "../HeroSection";
+import About from "../About";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <About />
+    </>
+  );
+}
+
+export default Home;
